7831455725c0fa0e8bcc7974934e880cc498392e
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-15 13:35:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(hourTime){
+  const pareNum = parseInt(hourTime);
+ if(pareNum < 12){
+   return ("Good Morning");
+ }
+ else if(pareNum >= 12 && pareNum <= 17){
+   return("Good Afternoon");
+ }
+ else{
+   return ("Good Evening");
+ }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(content){
+  document.getElementById("greeting").innerText=content;
+  
+  
+}
