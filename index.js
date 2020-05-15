9a777cd397adfab4d8dfc6ec3e1bef371e5f111f
Zainab Saud <zai99nb@gmail.com>
2020-05-15 10:37:49 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
+function greet(timeString) {
+  const e =parseInt(timeString);
+  if (e <12 ) {
+    return "Good Morning";
+  }
+  else if (e >12 && e < 17) {
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  let greeting = document.getElementById("greeting");
+  console.log(greeting);
+  greeting.innerText ="Good Morning";
+  greeting.innerText = "Good Afternoon";
+  greeting.innerText = "TEST";
+}
