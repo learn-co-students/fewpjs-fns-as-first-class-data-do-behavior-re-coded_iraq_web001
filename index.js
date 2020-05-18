2e3e66c4b93803134ad3d5a71045ac54f54ce190
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-18 02:37:22 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let hours = timeString.split(':')[0];
+
+
+
+  if (hours < 12) {
+    return "Good Morning"
+  }else if (hours <= 17) {
+    return "Good Afternoon"
+  }else  {
+    return "Good Evening"
+  }
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting){
+document.getElementById('greeting').innerText = greeting;
+}
