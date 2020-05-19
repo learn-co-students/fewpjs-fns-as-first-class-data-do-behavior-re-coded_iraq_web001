b8c3056c8fdf1895d9f411f2136e79a46c398d1a
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-05-19 19:28:53 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let thisTime = time.split(":");
+  let newTime = thisTime[0].concat(thisTime[1])
+  let result = parseInt(newTime,10)
+  
+  if (result < 1200){
+    return "Good Morning"
+  }
+  else if (result > 1700){
+    return "Good Evening"
+  }
+  else {
+    return "Good Afternoon"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+   let newmessage = document.getElementById("greeting");
+   newmessage.innerText = string;
+}
