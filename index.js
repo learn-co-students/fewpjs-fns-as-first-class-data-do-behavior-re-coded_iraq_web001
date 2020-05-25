959d8909de747a903a396a9b7c11093ecd30cfb2
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-05-26 02:55:11 +0300

    complete tha task

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = parseInt(document.getElementById("time").value);
+  // console.log(timeString);
+  // console.log(typeof timeString);
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time >= 12 && time < 17) {
+    return "Good Afternoon";
+  } else if (time >= 5) {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greeting) {
+  document.querySelector("#greeting").textContent = greeting;
+}
